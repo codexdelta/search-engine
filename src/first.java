6675a792c881ecd77005d1708c52683e2ac1dc86
ashwin <ashwin5devarajan@gmail.com>
2015-02-10 23:20:53 +0530

    updated stemmer

--- src/first.java
+++ src/first.java
@@ -1,16 +1,32 @@
 import java.util.*;
+import java.util.Map.Entry;
 import java.io.*;
+/**
+ * @author Aswin Devarajan
+ * 
+ *
+ */
 public class first {
-	public static void main(String[] args){
-		Scanner input = new Scanner(System.in);
-		token tokenObject = new token();
-		System.out.print("Enter your query: ");
-		String query = input.nextLine();
-		tokenObject.QuerytoToken(query);
-		System.out.println("thank you ");
-		fileScanner fileScannerObject = new fileScanner();
-		String[] a = null;
-		fileScannerObject.FilteredFileList(a);
-		
+/**
+ * @param args
+ */
+public static void main(String[] args)
+	{
+	Scanner input = new Scanner(System.in);
+	token tokenObject = new token();
+	fileScanner fileScannerObject = new fileScanner();
+	System.out.print("Enter your query: ");
+	String query = input.nextLine();
+	tokenObject.QuerytoToken(query);
+	
+	String[] a = null;
+	fileScannerObject.FilteredFileList(a);
+//    WordCount mdc = new WordCount();
+//    Map<String, Integer> wordMap = mdc.getWordCount("ashwin.txt");
+//    List<Entry<String, Integer>> list = mdc.sortByValue(wordMap);
+//    for(Map.Entry<String, Integer> entry:list){
+//        System.out.println(entry.getKey()+" ==== "+entry.getValue());
+//    }
+
 	}
 }
--- src/token.java
+++ src/token.java
@@ -3,11 +3,13 @@ import java.io.*;
 
 
 public class token {
+	public ArrayList<String> StemWords = new ArrayList<String>();
+	
 	public void QuerytoToken(String query){
 		int tokens = 0;
 		System.out.println("Your code is working!");
 		StringTokenizer st = new StringTokenizer(query, " ");
-		 ArrayList<String> al = new ArrayList<String>();
+		ArrayList<String> al = new ArrayList<String>();
 			while(st.hasMoreTokens()){
 				al.add(st.nextToken());	
 				
@@ -15,10 +17,65 @@ public class token {
 			}
 		System.out.println("Number of token are : " + tokens);
 		System.out.println(al);
+		
+		for(int x=0; x<al.size(); x++){
+			String stemTerm = al.get(x);
+			Stemmer(stemTerm);
+			
+		}
 	}
+		
+		//below code will work as stemmer	
 
+	private void Stemmer(String stemTerm) {
+		// TODO Auto-generated method stub
+		String tmp = stemTerm.toLowerCase();
+		int stemtermlength= tmp.length();
+		FileInputStream fis = null;
+		DataInputStream dis=null;
+		BufferedReader br= null;
+		
+		ArrayList<String> suffixList = new ArrayList<String>();
+		try{
+			fis = new FileInputStream("suffix.txt");
+			dis = new DataInputStream(fis);
+			br = new BufferedReader(new InputStreamReader(dis));
+			String line =null;
+			while((line = br.readLine()) != null){
+				StringTokenizer st = new StringTokenizer(line, " ");
+				 while(st.hasMoreTokens()){
+	                   String tmp1 = st.nextToken().toLowerCase();
+	                   if(tmp.endsWith(tmp1)){
+	                	  //strip off the suffix and check whether the new word is present in dic
+//	                	   System.out.println("length of the suffix:" + tmp1.length());
+	                	   String PrimaryStemWords =tmp.substring(0, tmp.length()-tmp1.length());
+	                	   DictionaryTokenCheck(PrimaryStemWords);
+//	                	   System.out.println(PrimaryStemWords);
+	               
+//	                	   System.out.println("got your term " + tmp1);
+	                	   
+	                   }
+	                  
+	 		      
+				 }
+		}
+			}catch (FileNotFoundException e) {
+	           e.printStackTrace();
+	       } catch (IOException e) {
+	           e.printStackTrace();
+	       } finally{
+	           try{if(br != null) br.close();}catch(Exception ex){}
+	       }
+		}
 	
-	public void WritetoFile(){
+	public void DictionaryTokenCheck(String PrimaryStemWords){
+		StemWords.add(PrimaryStemWords);
+		System.out.println(StemWords.get(0));
+		
 		
 	}
 }
+
+	       
+
+	
